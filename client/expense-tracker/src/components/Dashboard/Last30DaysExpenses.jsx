6f7f1ab1f0b3a5dccf34043ea26567f9bab25760
Nithin Rokkam <nithinrokkam@gmail.com>
2025-07-25 18:21:05 +0530

    Fix: CustomBarChart import path in Last30DaysExpenses

--- client/expense-tracker/src/components/Dashboard/Last30DaysExpenses.jsx
+++ client/expense-tracker/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { prepareExpenseBarChartData } from '../../utils/helper';
-import CustomBarChart from '../Charts/CustombarChart';
-const Last30DaysExpenses = ({data}) => {
-    const [chartData, setChartData] = useState([]);
-    useEffect(() => {
-        const result = prepareExpenseBarChartData(data);
-        setChartData(result);
+import CustomBarChart from '../Charts/CustomBarChart';
+const Last30DaysExpenses = ({ data }) => {
+  const [chartData, setChartData] = useState([]);
+  useEffect(() => {
+    const result = prepareExpenseBarChartData(data);
+    setChartData(result);
 
-        return () => {}
-    },[data]);
+    return () => { }
+  }, [data]);
   return (
     <div className='card col-span'>
-        <div className='flex items-center justify-between'>
-            <h5 className='text-lg'> Last 30 Days Expenses</h5>
-        </div>
+      <div className='flex items-center justify-between'>
+        <h5 className='text-lg'> Last 30 Days Expenses</h5>
+      </div>
 
-        <CustomBarChart data  = {chartData} />
+      <CustomBarChart data={chartData} />
     </div>
   )
 }
